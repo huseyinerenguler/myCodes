fc80c83916d3eded71493adf54eb3ef968935175
huseyinkuzeyguler <huseyinkuzey1997@gmail.com>
2017-04-15 21:59:24 +0300

    guncellendi

--- CENG104 - Computer Programming 2/HW3 - App of Assigning Advisor to Student/Project.cpp
+++ CENG104 - Computer Programming 2/HW3 - App of Assigning Advisor to Student/Project.cpp
@@ -6,6 +6,8 @@ using namespace std;
 void gotoxy(int x,int y){printf("%c[%d;%df", 0x1B, y, x);}
 void recordStudent(unsigned long,string,string,int,double,int);
 void recordAdvisor(unsigned long,string,string,int,int);
+void assignStudentToAdvisor(unsigned long);
+void editStudentRecord();
 
 class Advisor
 {
@@ -16,7 +18,8 @@ private:
     int numberOfStudentsAssigned; // number of students are assigned to advisor
     int allowedStudentCount;
 public:
-    void createAdvisor ()
+    Advisor(){}
+    void createAdvisor() // completed
     {
         system("clear");
         cout << "*************************************\n"
@@ -30,21 +33,81 @@ public:
 
         recordAdvisor(advisorId, firstName, lastName, numberOfStudentsAssigned, allowedStudentCount);
         cout << "A new advisor is created." << endl;
+        cout << "\nDo you want to add a new advisor? (Y/N): "; string choice; cin >> choice;
+        if (choice == "y" || choice == "Y") createAdvisor();
     }
-    void showAdvisorDetails ()
+    int showAdvisorList(int x) // completed
     {
+        system("clear");
+        cout << "********************** "
+             << " A D V I S O R   L I S T  "
+             << "*********************";
+        gotoxy(0,5); cout << "ADVISOR ID";
+        gotoxy(20,5); cout << "NAME";
+        gotoxy(34,5); cout << "SURNAME";
+        gotoxy(50,5); cout << "ASSIGNED STATUS" << endl;
+        cout << "----------------------------------------------------------------------";
+
+        ifstream file;
+        file.open("advisor.txt", ios::in);
+        for (int y = 7; !file.eof() == 1; y++)
+        {
+            file >> advisorId >> firstName >> lastName >> numberOfStudentsAssigned >> allowedStudentCount;
+            if (file.eof()) break; // son satiri iki kere yazdirmasini engelliyor
+
+            gotoxy(0,y); cout << advisorId;
+            gotoxy(20,y); cout << firstName;
+            gotoxy(34,y); cout << lastName;
+            gotoxy(50,y); cout << numberOfStudentsAssigned << " / "
+                               << allowedStudentCount << endl;
+        }
+        file.close();
+
+        if (x == 0)
+        {
+            gotoxy(0,3); cout << "0 <--Menu   1 <--Create Advisor   2 <--Search Advisor";
+            gotoxy(60,3); cout << "Choice: "; string choice = "0"; cin >> choice;
+            if (choice == "1") createAdvisor();
+            else if (choice == "2") showAdvisorDetails();
+            else if (choice != "0") showAdvisorList(0);
+        }
+        else if (x == 1)
+        {
+            gotoxy(0,3); cout << "Enter the ID of the advisor who you want to assigned: ";
+        }
+
+    }
+    void showAdvisorDetails() // completed
+    {
+        system("clear");
+        cout << "Enter the ID of the advisor who you want find: ";
+        unsigned long ID; cin >> ID;
+
+        int control = 0;
         ifstream file;
         file.open("advisor.txt", ios::in);
         while (!file.eof())
         {
-            file >> advisorId >> firstName >> lastName;
-            if (file.eof()) break; // son satiri iki kere yazdirmasini engelliyor
-            cout << advisorId << " " << firstName << " " << lastName << endl;
+            file >> advisorId >> firstName >> lastName >> numberOfStudentsAssigned >> allowedStudentCount;
+            if (advisorId == ID)
+            {
+                system("clear");
+                cout << "ID: " << advisorId
+                     << "\nName: " << firstName
+                     << "\nSurname: " << lastName << endl;
+                control = 1;
+            }
         }
         file.close();
+        if (control == 0) cout << "Advisor is not found !" << endl;
+        cout << "\n0 <-- Menu" << endl;
+        while (1)
+        {
+            cout << "Choice: "; string choice; cin >> choice;
+            if (choice == "0") break;
+        }
     }
 };
-
 class Student
 {
 private:
@@ -56,8 +119,8 @@ private:
     //Advisor advisor;
     int assignedFlag; // 0 or 1
 public:
-    Student (){}
-    void createStudent () // completed
+    Student(){}
+    void createStudent() // completed
     {
         system("clear");
         cout << "*************************************\n"
@@ -95,14 +158,14 @@ public:
         cout << "\nDo you want to add a new student? (Y/N): "; string choice; cin >> choice;
         if (choice == "y" || choice == "Y") createStudent();
     }
-    void showStudentList ()
+    void showStudentList() // completed
     {
         system("clear");
         cout << "********************** "
              << " S T U D E N T  L I S T  "
              << "*********************";
 
-        gotoxy(0,3); cout << "0 <-- Menu   1 <-- Create Student   2 <-- Search Student";
+        gotoxy(0,3); cout << "0 <--Menu    1 <--Create Student    2 <--Search Student";
         gotoxy(0,5); cout << "STUDENT ID";
         gotoxy(17,5); cout << "NAME";
         gotoxy(28,5); cout << "SURNAME";
@@ -131,7 +194,7 @@ public:
         else if (choice == "2") showStudentDetails();
         else if (choice != "0") showStudentList();
     }
-    void showStudentDetails()
+    void showStudentDetails() // completed
     {
         system("clear");
         cout << "Enter the ID of the student who you want find: ";
@@ -143,7 +206,6 @@ public:
         while (!file.eof())
         {
             file >> studentId >> firstName >> lastName >> year >> GPA >> assignedFlag;
-
             if (studentId == ID)
             {
                 system("clear");
@@ -159,23 +221,25 @@ public:
                 cout << "-------------------------------------" << endl;
                 currentStatus();
                 control = 1;
+                break;
             }
         }
         file.close();
         if (control == 0) cout << "Student is not found !" << endl;
-        cout << "\n0 <-- Menu" << endl;
+        cout << "\n0 <--Menu\n1 <--Assign Student to Advisor" << endl;
         while (1)
         {
             cout << "Choice: "; string choice; cin >> choice;
             if (choice == "0") break;
+            else if (choice == "1") assignStudentToAdvisor(studentId); break;
         }
     }
-    bool isSuccessful ()
+    bool isSuccessful() // completed
     {
         if (GPA >= 2.5) return true;
         else return false;
     }
-    void currentStatus ()
+    void currentStatus() // completed
     {
         if (isSuccessful())
             cout << "STUDENT: " << firstName << " " << lastName << " SUCCESSFUL" << endl;
@@ -183,7 +247,6 @@ public:
             cout << "STUDENT: " << firstName << " " << lastName << " NOT SUCCESSFUL" << endl;
     }
 };
-
 Student student;
 Advisor advisor;
 int main ()
@@ -195,33 +258,18 @@ int main ()
              << "2 <-- Show Advisor List\n"
              << "3 <-- Create Student\n"
              << "4 <-- Create Advisor\n"
+             << "5 <-- Assign Student to Advisor\n"
              << "0 <-- EXIT\n\n"
              << "Choice: ";
         int choice; cin >> choice;
-
         switch (choice)
         {
-            case 0: return 0;
-            case 1:
-            {
-                student.showStudentList();
-                break;
-            }
-            case 3:
-            {
-                student.createStudent();
-                break;
-            }
-            case 4:
-            {
-                char choice1 = 'y';
-                while (choice1 == 'y' || choice1 == 'Y')
-                {
-                    advisor.createAdvisor();
-                    cout << "\nDo you want to add a new advisor? (Y/N): "; cin >> choice1;
-                }
-                break;
-            }
+            case 0:return 0;
+            case 1:{student.showStudentList();break;}
+            case 2:{advisor.showAdvisorList(0);break;}
+            case 3:{student.createStudent();break;}
+            case 4:{advisor.createAdvisor();break;}
+            case 5:{student.showStudentDetails();break;}
         }
     }
     return 0;
@@ -242,7 +290,64 @@ void recordAdvisor (unsigned long id, string name, string surname, int numberOfS
          << numberOfStudentsAssigned << " " << allowedStudentCount << endl;
     file.close();
 }
-void assignStudentToAdvisor()
+void assignStudentToAdvisor(unsigned long sid)
 {
-    
+    advisor.showAdvisorList(1);
+    unsigned long aid; cin >> aid;
+    system("clear");
+
+    unsigned long advisorId; // ID of the advisor
+    string firstName; // first name of the advisor
+    string lastName; // last name of the advisor
+    int numberOfStudentsAssigned; // number of students are assigned to advisor
+    int allowedStudentCount;
+    int control = 0;
+
+    unsigned long studentId;
+    string sfirstName;
+    string slastName;
+    int year;
+    double GPA;
+    int assignedFlag;
+    //***************************************************************************************************
+    ifstream file1;
+    file1.open("advisor.txt", ios::in);
+    while (!file1.eof())
+    {
+        file1 >> advisorId >> firstName >> lastName >> numberOfStudentsAssigned >> allowedStudentCount;
+        if (file1.eof()) break;
+        if (advisorId == aid)
+        {
+            control = 1;
+            if (numberOfStudentsAssigned < allowedStudentCount) // assign islemi yapilacak
+            {
+                ifstream file2;
+                file2.open("student.txt", ios::in);
+                ofstream file3;
+                file3.open("yeni.txt", ios::app);
+                while(!file2.eof())
+                {
+                    file2 >> studentId >> sfirstName >> slastName >> year >> GPA >> assignedFlag;
+                    if (studentId == sid)
+                        assignedFlag = advisorId;
+                    if (file2.eof()) break;
+                    file3 << studentId << " " << sfirstName << " " << slastName << " "
+                         << year << " " << GPA << " " << assignedFlag << endl;
+                }
+                file2.close();
+                file3.close();
+                remove("student.txt");
+                rename("yeni.txt","student.txt");
+            }
+            else cout << "yapamazsin" << endl; // warning gelicek
+        }
+    }
+    file1.close();
+    if (control == 0) cout << "!!!Advisor is not found !" << endl;
+
+    cin >> control;
+}
+void editStudentRecord()
+{
+
 }
